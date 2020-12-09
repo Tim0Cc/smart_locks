a75c3668096e6edfbb97b7b3a8890c0985d7358e
Tim Cecatka <tim.cecatka@gmail.com>
2020-12-09 17:46:23 +0100

    fixed migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_153642) do
+ActiveRecord::Schema.define(version: 2020_12_09_161959) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,10 +26,9 @@ ActiveRecord::Schema.define(version: 2020_12_09_153642) do
 
   create_table "locks", id: :serial, force: :cascade do |t|
     t.string "kind"
-    t.boolean "status_change"
+    t.string "status"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "lock_id"
   end
 
   create_table "servers", force: :cascade do |t|
